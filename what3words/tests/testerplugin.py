cdd01ac37a423e249d07bd6ec3e5403dc8f514e8
volaya <volayaf@gmail.com>
2016-03-22 09:54:33 +0100

    added tests

--- what3words/tests/testerplugin.py
+++ what3words/tests/testerplugin.py
@@ -1,16 +1,36 @@
 # Tests for the QGIS Tester plugin. To know more see
 # https://github.com/boundlessgeo/qgis-tester-plugin
 
+from qgis.utils import *
+from qgis.core import *
 
 def functionalTests():
     try:
         from qgistester.test import Test
-        from qgistester.utils import *
+        from qgistester.utils import loadLayer
     except:
         return []
 
-    #write tests here and return them
-    return []
+    def _loadLayer():
+        layerfile = os.path.join(os.path.dirname(__file__), "w3w.shp")
+        layer = loadLayer(layerfile)
+        QgsMapLayerRegistry.instance().addMapLayer(layer)
+
+    def _setTool():
+        plugins["what3words"].setTool()
+
+    def _zoomTo():
+        plugins["what3words"].zoomTo()
+
+    w3wTest = Test("Test w3w")
+    w3wTest.addStep("Load layer", _loadLayer)
+    w3wTest.addStep("Select map tool", _setTool)
+    w3wTest.addStep("Click within the layer polygon and verify that the computed 3 coords are 'healings.distorting.harsher'", isVerifyStep = True)
+    w3wTest.addStep("Move map canvas", lambda: iface.mapCanvas().setCenter(QgsPoint(0, 0)))
+    w3wTest.addStep("Open panel", _zoomTo)
+    w3wTest.addStep("Enter 'healings.distorting.harsher' and click on 'zoom to'. Verify it zooms to the polygon layer")
+
+    return [w3wTest]
 
 
 def unitTests():
