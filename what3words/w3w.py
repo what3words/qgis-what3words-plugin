9251bac8e4abd1678d8f1518fb841bf41e30e8ec
vicchi <gary@vicchi.org>
2016-06-15 11:33:25 +0100

    Update plugin to use what3words v2 API, plus minor labelling corrections and fixes

--- what3words/w3w.py
+++ what3words/w3w.py
@@ -28,33 +28,28 @@ class what3words(object):
     """what3words API"""
 
     def __init__(self, host='api.what3words.com', apikey=''):
-        self.host = 'http://' + host
+        self.host = 'https://' + host
         self.apikey = apikey
 
-    def getPosition(self, words='*libertytech', onewordpassword='',
-                    lang='en', corners='false', email='', password=''):
+    def forwardGeocode(self, words='index.home.raft', lang='en'):
         if isinstance(words, list):
             words = "%s.%s.%s" % (words[0], words[1], words[2])
-            print "string is: %s" % (words)
-        params = { 'corners': corners, 'string': words }
-        if (onewordpassword != ''):
-            params.update({'onewordpassword', onewordpassword,
-                           'email', email,
-                           'password', password});
-        return self.postRequest(self.host + '/w3w', params)
+        params = {'addr':words, 'display':'full', 'format':'json', 'lang':lang}
+        return self.postRequest(self.host + '/v2/forward', params)
 
-    def getWords(self, lat='', lng='', corners='false', lang='en'):
-        position = "%s,%s" % (lat, lng)
-        params = { 'position': position, 'corners': corners, 'lang': lang }
-        return self.postRequest(self.host + '/position', params)
+    def reverseGeocode(self, lat='', lng='', corners='false', lang='en'):
+        coords = "%s,%s" % (lat, lng)
+        params = {'coords':coords, 'display':'full', 'format':'json', 'lang':lang}
+        return self.postRequest(self.host + '/v2/reverse', params)
 
     def getLanguages(self):
-        return self.postRequest(self.host + '/get-languages', dict())
+        return self.postRequest(self.host + '/v2/languages', dict())
 
     def postRequest(self, url, params):
         params.update({'key': self.apikey})
         encparams = urllib.urlencode(params)
-        try: response = urllib.urlopen(url, encparams).read()
+        url = url + '?' + encparams
+        try: response = urllib.urlopen(url).read()
         except URLError as e:
             raise e
         return json.loads(response)
